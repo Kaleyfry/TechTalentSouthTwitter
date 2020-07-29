658f2b572420cf000044c992f3fa2823606416cc
Kaleyfry <59694186+Kaleyfry@users.noreply.github.com>
2020-07-29 16:29:18 -0400

    Updated to add lombok to model classes

--- src/main/java/com/tts/TechTalentTwitter/controller/FollowController.java
+++ src/main/java/com/tts/TechTalentTwitter/controller/FollowController.java
@@ -14,6 +14,7 @@ import com.tts.TechTalentTwitter.service.UserService;
 
 @Controller
 public class FollowController {
+
     @Autowired
     private UserService userService;
 
--- src/main/java/com/tts/TechTalentTwitter/model/Role.java
+++ src/main/java/com/tts/TechTalentTwitter/model/Role.java
@@ -1,16 +1,16 @@
 package com.tts.TechTalentTwitter.model;
 
-// import lombok.AllArgsConstructor;
-// import lombok.Builder;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-// @Data
-// @Builder
-// @AllArgsConstructor
-// @NoArgsConstructor
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Role {
     @Id
@@ -19,22 +19,22 @@ public class Role {
     private Long id;
     private String role;
 
+    // Use this code if your lombok is not working:
+    // public String getRole() {
+    // return role;
+    // }
 
-    public String getRole() {
-        return role;
-    }
+    // public void setRole(String role) {
+    // this.role = role;
+    // }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+    // public Long getId() {
+    // return id;
+    // }
 
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "Role [id=" + id + ", role=" + role + "]";
-    }
+    // @Override
+    // public String toString() {
+    // return "Role [id=" + id + ", role=" + role + "]";
+    // }
 
 }
